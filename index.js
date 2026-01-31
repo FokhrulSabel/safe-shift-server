086f6e5e4e4e466ee341132f99bcd87c5164949b
FokhrulSabel <fokhrulislam316@gmail.com>
2026-02-01 00:39:22 +0600

    added parcel delete api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -52,7 +52,6 @@ async function run() {
 
     app.post("/parcels", async (req, res) => {
       const parcel = req.body;
-
       // parcel created time
       parcel.createdAt = new Date();
 
@@ -60,6 +59,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/parcels/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+
+      const result = await parcelsCollection.deleteOne(query);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
